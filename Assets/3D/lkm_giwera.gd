e4d27a8c96d1dc267338885a237ea76677a62b64
F1ejm <147535168+F1ejm@users.noreply.github.com>
2025-03-14 16:48:27 -0700

    lkm dziala

--- Assets/3D/lkm_giwera.gd
+++ Assets/3D/lkm_giwera.gd
@@ -5,9 +5,9 @@ var anim = false
 func _process(delta: float) -> void:
 	if Input.is_action_pressed("strzał"):
 		$AnimationPlayer.play("Cylinder_003Action")
-		$"../Timer".start()
+#		$"../Timer".start()
 #		$AnimationPlayer.play("Cylinder_003Action")
 
 
-func _on_timer_timeout() -> void:
-	$AnimationPlayer.play("Cylinder_003Action")
+#func _on_timer_timeout() -> void:
+#	$AnimationPlayer.play("Cylinder_003Action")
