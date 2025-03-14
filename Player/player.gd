eddb4219d3aac41370ff3939f3b135028abf5175
Gigabajt000 <nikitadyrcz8@gmail.com>
2025-03-14 18:46:38 +0100

    slismk postacien

--- Player/player.gd
+++ Player/player.gd
@@ -21,6 +21,7 @@ var current_speed = 5.0
 var slide_timer = 0.0
 var slide_timer_max = 1.0
 var slide_vector = Vector2.ZERO
+var slide_speed = 10.0
 
 const crouch_depth = -0.5
 
@@ -40,7 +41,10 @@ func _ready() -> void:
 	
 func _input(event: InputEvent) -> void:
 	if event is InputEventMouseMotion:
-		rotate_y(deg_to_rad(-event.relative.x * mouse_sens))
+		if freelooking:
+			neck.rotate_y(deg_to_rad(-event.relative.x * mouse_sens))
+		else:
+			rotate_y(deg_to_rad(-event.relative.x * mouse_sens))
 		head.rotate_x(deg_to_rad(-event.relative.y * mouse_sens))
 		head.rotation.x = clamp(head.rotation.x, deg_to_rad(-89), deg_to_rad(89))
 	
@@ -53,9 +57,10 @@ func _physics_process(delta: float) -> void:
 
 	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = jump_force
+		sliding = false
 
 # handle crouch
-	if Input.is_action_pressed("crouch"):
+	if Input.is_action_pressed("crouch") or sliding:
 		current_speed = crouch_speed
 		
 		head.position.y = move_toward(head.position.y, crouch_depth, delta * 3)
@@ -67,7 +72,6 @@ func _physics_process(delta: float) -> void:
 			sliding = true
 			slide_timer = slide_timer_max
 			slide_vector = input_dir
-			print("start")
 		
 		crouching = true
 		walking = false
@@ -93,13 +97,13 @@ func _physics_process(delta: float) -> void:
 			walking = true
 			sprinting = false
 			
-	#Handle free looking 
+	#Handle free looking || FREELOOK KURWO
 	if sliding == true: 
 		freelooking = true
-		camera_3d.rotation.z = deg_to_rad(neck.rotation.y * free_look_tilt_amount)
+		camera_3d.rotation.z = move_toward(camera_3d.rotation.z, -deg_to_rad(7.0), delta)
 	else:
 		freelooking = false
-		neck.rotation.y = move_toward(neck.rotation.y, 0.0, delta)
+		neck.rotation.y = move_toward(neck.rotation.y, 0.0, delta * 10)
 		camera_3d.rotation.z = move_toward(camera_3d.rotation.z,0.0, delta)
 	
 	
@@ -109,20 +113,20 @@ func _physics_process(delta: float) -> void:
 		slide_timer -= delta
 		if slide_timer <= 0:
 			sliding = false
-			print("end")
+			freelooking = false
 
 	var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	
 	if sliding:
-		direction = (transform.basis * Vector3(slide_vector.x, 0, slide_vector.z)).normalized()
+		direction = (transform.basis * Vector3(slide_vector.x, 0, slide_vector.y)).normalized()
 	
 	if direction:
 		velocity.x = direction.x * current_speed
 		velocity.z = direction.z * current_speed
 		
 		if sliding:
-			velocity.x = direction.x * slide_timer
-			velocity.z = direction.z * slide_timer
+			velocity.x = direction.x * (slide_timer + 0.2) * slide_speed
+			velocity.z = direction.z * (slide_timer + 0.2) * slide_speed
 			
 	else:
 		velocity.x = move_toward(velocity.x, 0, current_speed)
