f197311e60b23354544f110fd93e8215f02fa76c
mxels <mikolajmeg07@gmail.com>
2025-03-15 07:57:43 +0100

    ble

--- UI/Main Menu Etc/main_menu.gd
+++ UI/Main Menu Etc/main_menu.gd
@@ -21,3 +21,7 @@ func _on_settings_pressed():
 
 func _on_quit_pressed():
 	get_tree().quit()
+
+
+func _on_instructions_pressed() -> void:
+	get_tree().change_scene_to_file("res://Levels/instructions.tscn")
