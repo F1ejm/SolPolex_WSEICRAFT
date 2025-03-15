accf683309e42779ac2c528528134586f52d3e0f
F1ejm <147535168+F1ejm@users.noreply.github.com>
2025-03-14 18:23:22 -0700

    Revert "KAROL ZRUB"
    This reverts commit 30e95809aaf6f24291178313d23b0db981d669f8.

--- jiglly/Scene/sad/sjkafsdfjhavsddghf/slime.gd
+++ jiglly/Scene/sad/sjkafsdfjhavsddghf/slime.gd
@@ -1,4 +1,4 @@
-extends RigidBody3D
+extends CharacterBody3D
 
 @export var MoveSpeed:float = 4.0
 @onready var navigation_agent : NavigationAgent3D = $NavigationAgent3D
@@ -8,14 +8,14 @@ var player: CharacterBody3D = null
 
 func _ready() -> void:
 	player = get_tree().get_nodes_in_group("player")[0]
-	linear_velocity = Vector3(1,0,1)
+	velocity = Vector3(1,0,1)
 
 func _physics_process(delta: float) -> void:
-	
-	if linear_velocity.y >= 0:
-		gravity_scale = 2
-	else:
-		gravity_scale = 4
+	if not is_on_floor():
+		if velocity.y >= 0:
+			velocity += get_gravity() * delta * 3
+		else:
+			velocity += get_gravity() * delta * 6
 	
 	navigation_agent.set_target_position(player.global_position)
 	
@@ -23,10 +23,10 @@ func _physics_process(delta: float) -> void:
 		return
 	
 	var next_position: Vector3 = navigation_agent.get_next_path_position()
-	if abs(linear_velocity.z) < abs(next_position.z * MoveSpeed) and abs(linear_velocity.x) < abs(next_position.x * MoveSpeed):
-		linear_velocity = global_position.direction_to(next_position)  *  MoveSpeed
+	if abs(velocity.z) < abs(next_position.z * MoveSpeed) and abs(velocity.x) < abs(next_position.x * MoveSpeed):
+		velocity = global_position.direction_to(next_position)  *  MoveSpeed
 
-	
+	move_and_slide()
 	
 
 func _on_area_3d_body_entered(body: Node3D) -> void:
