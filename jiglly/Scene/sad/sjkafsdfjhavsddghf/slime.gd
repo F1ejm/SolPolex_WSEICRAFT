f31a32e4ec0250c230ee18db966a22b2ce8dfe43
Kubuz520 <witcherandmaster@op.pl>
2025-03-15 05:56:53 +0100

    zgfz

--- jiglly/Scene/sad/sjkafsdfjhavsddghf/slime.gd
+++ jiglly/Scene/sad/sjkafsdfjhavsddghf/slime.gd
@@ -6,11 +6,20 @@ var player: CharacterBody3D = null
 
 @onready var srodek: Node3D = $Srodek
 
+var timer:float = 0.8
+var start_timer: bool = false
+
 func _ready() -> void:
 	player = get_tree().get_nodes_in_group("player")[0]
 	velocity = Vector3(1,0,1)
 
 func _physics_process(delta: float) -> void:
+	if start_timer == true:
+		timer -= delta
+		
+	if timer <= 0:
+		timer = 0.8
+		Atak()
 	if not is_on_floor():
 		if velocity.y >= 0:
 			velocity += get_gravity() * delta * 3
@@ -29,6 +38,20 @@ func _physics_process(delta: float) -> void:
 	
 
 func _on_area_3d_body_entered(body: Node3D) -> void:
-	pass
+	if body.is_in_group("player"):
+		start_timer = true
+	
+	
+func Atak():
+	GlobalWeaponTimer.Życie -= 10
+	$CPUParticles3D.global_position = player.global_position
+	$CPUParticles3D.emitting = true
+	
 func test (a):
 	velocity=a
+
+
+func _on_area_3d_body_exited(body: Node3D) -> void:
+	if body.is_in_group("player"):
+		start_timer = false
+		timer = 0.8
